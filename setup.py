1ef3907f3d56f9fd7c78f4ac435da12a2a6a5d22
Manoj M Wagle <74168582+manojmw@users.noreply.github.com>
2026-07-11 04:57:57 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+##############################################
+
+# Manoj M Wagle (USydney; MIT CSAIL; Broad Institute)
+
+##############################################
+
+
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name="Phenoverse",
+    version="0.1.0",
+    packages=find_namespace_packages(),
+    python_requires='>=3.12',
+    install_requires=[
+        'torch',
+        'scanpy',
+        'pandas',
+        'numpy',
+        'scikit-learn',
+    ],
+    entry_points={
+        'console_scripts': [
+            'phenoverse=phenoverse.Phenoverse:main',
+        ],
+    },
+    include_package_data=True,
+    description='Deep interpretable learning of sample representations for characterizing disease states in single-cell transcriptomics',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/KellisLab/Phenoverse',
+    project_urls={
+        'Documentation': 'https://kellislab.github.io/Phenoverse/',
+    },
+    author='Manoj M Wagle',
+    author_email='manojmw@mit.edu',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.12',
+    ],
+)
